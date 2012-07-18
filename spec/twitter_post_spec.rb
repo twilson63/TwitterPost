ce2a59469ad8f1ce9b164f57da395f3eac4a6a01
Tom Wilson <tom@jackhq.com>
2012-07-18 16:01:01 -0400

    assume it is working

--- spec/twitter_post_spec.rb
+++ spec/twitter_post_spec.rb
@@ -4,4 +4,7 @@ describe TwitterPost do
   it 'should be true' do
     subject.should be_true
   end
+  it 'should post tweet' do
+    subject.tweet 'Hello World!'
+  end
 end
--- twitter_post.rb
+++ twitter_post.rb
@@ -2,5 +2,14 @@ require 'twitter'
 
 class TwitterPost
 
-
+  def tweet(msg)
+    Twitter.configure do |config|
+      config.consumer_key = 'A1hNVADC3MzhbdsVtVWrA'
+      config.consumer_secret = 'SoIogk9DNKiRw6Qb5zH4sLoSV9095hFiHxKWNQi18k'
+      config.oauth_token = '125307765-6fwTSh5YHj1HdAUtwUeSwx3nwuDw47WTjDfMswQf'
+      config.oauth_token_secret = 'IJFQ7JsJiJFwfX4o76vMiWJXo4jPBFmdeBcqLaM'
+    end
+    
+    Twitter.update(msg)
+  end
 end
